7f1ff6f4bd6cac460cfe6dde119b10656c8c9631
fernandocto.scz <fernandocto.scz@gmail.com>
2019-07-21 20:11:45 -0400

    Change config firebase

--- src/utils/firebase/config.js
+++ src/utils/firebase/config.js
@@ -1,9 +1,9 @@
 export default firebaseConfig = {
-  apiKey: "AIzaSyClMOf_hG7YtLn0v0FMOkJWZLR0C5lAPek",
-  authDomain: "busmap-487c2.firebaseapp.com",
-  databaseURL: "https://busmap-487c2.firebaseio.com",
-  projectId: "busmap-487c2",
-  storageBucket: "busmap-487c2.appspot.com",
-  messagingSenderId: "509716669696",
-  appId: "1:509716669696:web:59be84861d8f3ffa"
+  apiKey: "AIzaSyCt2pNTsJDf8FIVN_YZH644-9wsdlgq4Do",
+  authDomain: "some-restaurant.firebaseapp.com",
+  databaseURL: "https://some-restaurant.firebaseio.com",
+  projectId: "some-restaurant",
+  storageBucket: "",
+  messagingSenderId: "916185589083",
+  appId: "1:916185589083:web:9e27bd190692e8f8"
 }
