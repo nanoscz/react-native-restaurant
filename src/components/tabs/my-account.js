73913e05b175d75eb2d8f42cbc0834b64d64259a
fernandocto.scz <fernandocto.scz@gmail.com>
2019-07-21 22:01:50 -0400

    Sign In and sign Out

--- src/components/tabs/my-account.js
+++ src/components/tabs/my-account.js
@@ -1,20 +1,64 @@
 import React from 'react'
 import { StyleSheet, View, Text } from 'react-native'
-import { Button }  from 'react-native-elements'
+import { Button } from 'react-native-elements'
+/** Firebase */
+import * as  firebase from 'firebase'
+
 export default class MyAccount extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      login: false
+    }
+  }
+
+  async componentDidMount() {
+    await firebase.auth().onAuthStateChanged(user => {
+      console.log("user", user)
+      if (!user) {
+        this.setState({ login: false })
+      } else {
+        this.setState({ login: true })
+      }
+    })
+  }
 
   goToRegister = (namePage) => {
     this.props.navigation.navigate(namePage)
   }
 
+  onLogout = () => {
+    try {
+      firebase.auth().signOut();
+      console.log("logout")
+    } catch (error) {
+      this.handlerError(error)
+    }
+  }
+
+  handlerError(error) {
+    console.log(error)
+  }
+
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>MyAccount</Text>
-        <Button title='Login' onPress={()=>this.goToRegister('Login')}/>
-        <Button title='Register' onPress={()=>this.goToRegister('Register')}/>
-      </View>
-    );
+    if (this.state.login) {
+      return (
+        <View>
+          <Text> logeado..</Text>
+          <Button title='Logout' onPress={() => this.onLogout()} />
+        </View>
+      )
+    } else {
+      return (
+        <View style={styles.container}>
+          <Text>MyAccount</Text>
+          <Button title='Login' onPress={() => this.goToRegister('Login')} />
+          <Button title='Register' onPress={() => this.goToRegister('Register')} />
+        </View>
+      );
+    }
   }
 }
 
